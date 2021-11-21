a7bd20f632a1862294d5bcb77e7864da230be0ad
Ronaldo Gonçalves Nunes <ronaldo2385nunes@gmail.com>
2021-11-21 14:17:40 -0300

    adicionando novas classe cadastro api

---
+++ DeliveryApis/src/DeliveryCadastro/Dtos/EndereçoDto.cs
@@ -0,0 +1,39 @@
+﻿namespace DeliveryCadastro.Dtos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EndereçoDto
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Rua { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Numero { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Municipio { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Bairro { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Complemento { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Cep { get; set; }
+
+    }
+}
---
+++ DeliveryApis/src/DeliveryCadastro/Dtos/UsuarioDto.cs
@@ -0,0 +1,25 @@
+﻿namespace DeliveryCadastro.Dtos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UsuarioDto
+    {
+        /// <summary>
+        /// Nome completo do usuario
+        /// </summary>
+        public string? NomeCompleto { get; set; }
+
+        /// <summary>
+        /// e-mail do usuario 
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// senha usuario
+        /// </summary>
+        public string? Senha { get; set; }
+
+        public EndereçoDto? Endereco { get; set; }
+    }
+}
