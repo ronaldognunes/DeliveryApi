aa108ccc3152f411322115daf4dcb828c02ef8ca
Ronaldo Gonçalves Nunes <ronaldo2385nunes@gmail.com>
2021-12-03 22:07:19 -0300

    classes

--- DeliveryApis/src/DeliveryCatalogo/Entities/Categoria.cs
+++ DeliveryApis/src/DeliveryCatalogo/Entities/Categoria.cs
@@ -2,8 +2,17 @@
 {
     public class Categoria
     {
+        public Categoria(Guid id, string descricao, string nomeCategoria) 
+        {
+            Id = id;
+            Descricao = descricao;
+            NomeCategoria = nomeCategoria;
+   
+        }
         public Guid Id { get; private set; }
         public string NomeCategoria { get; private set; }
         public string Descricao { get; private set; }
+        
     }
+
 }
