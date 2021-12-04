185677f734308dbb86c3ed7ab8995d4e67375416
Ronaldo Gonçalves Nunes <ronaldo2385nunes@gmail.com>
2021-12-03 22:04:25 -0300

    classes

--- DeliveryApis/src/DeliveryCatalogo/Entities/Produto.cs
+++ DeliveryApis/src/DeliveryCatalogo/Entities/Produto.cs
@@ -2,11 +2,19 @@
 {
     public class Produto
     {
+        public Produto(Guid id,string nomeProduto, string descricao,decimal valor, Categoria categoriaProduto)
+        {
+            Id = id;
+            Descricao = descricao;
+            CategoriaProduto = categoriaProduto;
+            Valor = valor;
+            NomeProduto = nomeProduto;
+        }
         public Guid Id { get; private set; }
         public string NomeProduto { get; private set; }
         public string Descricao { get; private set; }
         public decimal Valor { get; private set; }
-        public Categoria CategoriaProduto { get;private set; }
+        public Categoria CategoriaProduto { get; private set; }
 
     }
 }
